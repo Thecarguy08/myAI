34e3c2e9be74d5bfb157de85caa983d35f00165a
Thecarguy08 <conner.whitt08@gmail.com>
2025-03-07 09:57:15 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. Please remember that while using this tool is free, that also means that your privacy CANNOT be guaranteed. Make sure you do not talk or ask about anything personal (ex. finances, social security number, phone number, and address).`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. While this tool is free, absolute privacy cannot be guaranteed. For your protection, kindly avoid sharing any sensitive personal information such as financial details, social security numbers, phone numbers, or addresses.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
