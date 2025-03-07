cf16ef874cece3f8e334ddadd94b4649f69f94e0
Thecarguy08 <conner.whitt08@gmail.com>
2025-03-07 09:51:10 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Welcome to BizPlanBot`;
+export const CHAT_HEADER: string = `Welcome to Culture Catalyst`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Made with love by Conner W. @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `Clear Text!`;
-export const PAGE_TITLE: string = `BizPlanBot`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Text :)`;
+export const PAGE_TITLE: string = `CultureCatalyst`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
