357850e41d5313b516b68eda1deea025eb71052f
Thecarguy08 <conner.whitt08@gmail.com>
2025-03-07 09:49:45 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Conner`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `CultureCatalyst`;
+export const AI_NAME: string = `Culture Catalyst`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
